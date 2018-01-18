326bac7ae2e3f4349575aea60e338d595e0110d3
android-genora <android@genora.co.uk>
2018-01-18 14:34:45 +0530

    Commit 1.2 Made a RecyclerView for Publishers in Kotlin..

--- app/src/main/java/genora/example/com/breader/activity/MainActivity.kt
+++ app/src/main/java/genora/example/com/breader/activity/MainActivity.kt
@@ -2,12 +2,59 @@ package genora.example.com.breader.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import genora.example.com.breader.R
+import genora.example.com.breader.adapter.PublisherAdapter
+import genora.example.com.breader.model.Publisher
+import kotlinx.android.synthetic.main.recycler_view_layout.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var publisherAdapter: PublisherAdapter
+    private var publisherList: ArrayList<Publisher> = ArrayList<Publisher>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        publisherList = addNewPublishers()
+
+        publisherAdapter = PublisherAdapter(this, publisherList)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = linearLayoutManager
+        recycler_view.adapter = publisherAdapter
+    }
+
+    private fun addNewPublishers(): ArrayList<Publisher> {
+        var publisher: Publisher
+
+        publisher = Publisher(1, "Sonia W", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        publisher = Publisher(1, "Shilpa W", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        publisher = Publisher(1, "Krupa Bhat", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        publisher = Publisher(1, "Stephen G", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        publisher = Publisher(1, "Suhail S", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        publisher = Publisher(1, "Saisha N", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        publisher = Publisher(1, "Vaishali S", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        publisher = Publisher(1, "Lax N", "blah blah", "11-11-2018")
+        publisherList.add(publisher)
+
+        return publisherList
     }
 }
--- app/src/main/java/genora/example/com/breader/adapter/PublisherAdapter.kt
+++ app/src/main/java/genora/example/com/breader/adapter/PublisherAdapter.kt
@@ -1,7 +1,37 @@
 package genora.example.com.breader.adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import genora.example.com.breader.R
+import genora.example.com.breader.model.Publisher
+import kotlinx.android.synthetic.main.publisher_item_layout.view.*
+
 /**
  * Created by genora-sonia on 16/1/18.
  */
-class PublisherAdapter {
+class PublisherAdapter(private val context: Context, private var publisherList: List<Publisher>)
+    : RecyclerView.Adapter<PublisherAdapter.PublisherHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): PublisherHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.publisher_item_layout, parent, false)
+        return PublisherHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PublisherHolder?, position: Int) {
+        val publisher = publisherList[position]
+        holder?.itemView?.publisher_name?.text = publisher.publisher_name
+        holder?.itemView?.publisher_timestamp?.text = publisher.publisher_timestamp
+       // Glide.with(context).load(publisher.publisher_icon)
+         //       .into(holder?.itemView?.publisher_icon!!)
+    }
+
+    override fun getItemCount() = publisherList.size
+
+    class PublisherHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        // private var view = itemView
+    }
 }
